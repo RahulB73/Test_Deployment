dcd79b1c57b526dff0faa51c6b8f7ad96b4fcda8
Rahul Suresh Borkar <rahulborkar2001@gmail.com>
2024-05-23 19:14:39 +0530

    test_deployment

---
+++ app/pages/login/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import axios from "axios";
+import { useState } from "react";
+
+
+const LoginPage = () => {
+    const [inputs, setInputs] = useState({});
+
+    const handleInput = (e: { target: { name: any; value: any; }; }) => {
+        setInputs((prev) => {
+            return { ...prev, [e.target.name]: e.target.value };
+        });
+    };
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("", inputs)
+            console.log(res)
+            alert("You have been logged in ..")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="">Email :
+                    <input type="text" name="email" onChange={handleInput} />
+                </label>
+                <label htmlFor="">Password :
+                    <input type="password" name="password" onChange={handleInput} />
+                </label>
+                <button onClick={handleSubmit}> Submit </button>
+            </form>
+        </div>
+
+    );
+}
+
+export default LoginPage;
---
+++ app/pages/signup/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import axios from "axios";
+import { useState } from "react";
+
+
+const SignupPage = () => {
+    const [inputs, setInputs] = useState({});
+
+    const handleInput = (e: { target: { name: any; value: any; }; }) => {
+        setInputs((prev) => {
+            return { ...prev, [e.target.name]: e.target.value };
+        });
+    };
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("http://test-deployment-nu.vercel.app/api/signup", inputs)
+            console.log(res)
+            alert("You have been sign in ..")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Signup Page</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="">Client Name :
+                    <input type="text" name="clientName" onChange={handleInput} />
+                </label>
+                <label htmlFor="">Email :
+                    <input type="text" name="email" onChange={handleInput} />
+                </label>
+                <label htmlFor="">Password :
+                    <input type="password" name="password" onChange={handleInput} />
+                </label>
+                <button onClick={handleSubmit}> Submit </button>
+            </form>
+        </div>
+
+    );
+}
+
+export default SignupPage;
